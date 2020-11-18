78a75ae89d621ae1ea94c14b82ed2c16d06d55d8
Zolo101 <umadeguy@gmail.com>
2020-11-18 16:54:31 +0000

    Replace level format with Tiled JSON

--- src/game/core/levelstructure.ts
+++ src/game/core/levelstructure.ts
@@ -7,21 +7,51 @@ export type Entity = {
     roleid?: number
 }
 
+export type EntityObject = {
+    name: string
+    type: string
+    id: number
+    gid: number // Global ID
+    width: number
+    height: number
+    visible: boolean
+    rotation: number
+    x: number
+    y: number
+}
+
 export type Dialogue = {
-    name: string,
+    name: string
     happy: boolean
     text: string
 }
 
-export type Level = {
-    name: string
-    width: number
-    height: number
-    background: number
-    data: string[]
-    entity: Entity[]
-    dialogue: Dialogue[]
-}
+export type Level = [
+        {
+            name: string
+            width: number
+            height: number
+            data: number[]
+            opacity: number
+            type: string
+            visible: boolean
+            id: number
+            x: number
+            y: number
+        },
+        {
+            name: string
+            id: number
+            draworder: string
+            objects: EntityObject[]
+            opacity: number
+            type: string
+            visible: boolean
+            x: number
+            y: number
+        }
+]
+
 
 export type LevelData = {
     name: string
