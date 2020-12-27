d929f108db2af0bd888ed95ca191c9d1c4506237
Zolo101 <umadeguy@gmail.com>
2020-12-27 23:39:36 +0000

    Improved Explore

--- src/scenes/explore.ts
+++ src/scenes/explore.ts
@@ -8,7 +8,7 @@ let infoText: Phaser.GameObjects.Text;
 
 const exploreButtonStyle = {
     fontFamily: "Helvetica, Arial, sans-serif",
-    fontSize: "42px",
+    fontSize: "28px",
     fontStyle: "bold",
     align: "center",
     // fixedWidth: 100,
@@ -22,7 +22,7 @@ const exploreButtonStyle = {
 
 const levelnameButtonStyle = {
     fontFamily: "Helvetica, Arial, sans-serif",
-    fontSize: "24px",
+    fontSize: "28px",
     backgroundColor: "#444",
     padding: {
         y: 4,
@@ -32,7 +32,7 @@ const levelnameButtonStyle = {
 
 const helpButtonStyle = {
     fontFamily: "Helvetica, Arial, sans-serif",
-    fontSize: "42px",
+    fontSize: "28px",
     fontStyle: "bold",
     align: "center",
     backgroundColor: "#336",
@@ -85,7 +85,10 @@ class exploreScene extends Phaser.Scene {
         // Background
         this.add.rectangle(0, 0, 960, 540, 0x375342).setOrigin(0, 0);
 
-        epochtimetext = this.add.text(0, 0, "awaiting time...", textStyle).setFontSize(32);
+        epochtimetext = this.add.text(0, 0, "awaiting time...", textStyle)
+            .setFontSize(32)
+            .setFontStyle("bold")
+            .setColor("#507860");
 
         const backButton = this.add.text(
             800, 475, "BACK", backStyle,
@@ -96,24 +99,24 @@ class exploreScene extends Phaser.Scene {
             this.scene.start("menuScene");
         });
 
-        /* const featuredbutton = this.add.text(
+        const featuredbutton = this.add.text(
             30, 50, "FEATURED", exploreButtonStyle,
         );
 
         const newbutton = this.add.text(
-            389, 50, "NEW", exploreButtonStyle,
+            340, 50, "NEW", exploreButtonStyle,
         );
 
         const topbutton = this.add.text(
-            620, 50, "TOP", exploreButtonStyle,
+            580, 50, "TOP", exploreButtonStyle,
         );
 
-        const helpbutton = this.add.text(
-            860, 50, "?", helpButtonStyle,
-        ); */
+        //const helpbutton = this.add.text(
+        //    860, 50, "?", helpButtonStyle,
+        //).setFontSize(42);
 
         const refreshbutton = this.add.text(
-            10, 475, "REFRESH", helpButtonStyle,
+            30, 475, "REFRESH", helpButtonStyle,
         ).setInteractive();
 
         refreshbutton.on("pointerdown", () => {
@@ -121,35 +124,38 @@ class exploreScene extends Phaser.Scene {
         });
 
         const fivebeambutton = this.add.text(
-            260, 475, "5BEAM", helpButtonStyle,
+            220, 475, "WEBSITE", helpButtonStyle,
         ).setInteractive().setBackgroundColor("#476");
 
         fivebeambutton.on("pointerdown", () => {
-            openExternalLink("https://5beam.zapto.org/");
+            openExternalLink("https://5beam.zelo.dev/");
         });
 
+        /*
         const tutorialbutton = this.add.text(
             460, 475, "TUTORIAL", helpButtonStyle,
         ).setInteractive().setBackgroundColor("#b64");
 
         tutorialbutton.on("pointerdown", () => {
             openExternalLink("https://gist.github.com/Zolo101/36ae33e5dd15510a2cb41e942dbf7044");
-        });
+        }); */
 
         infoText = this.add.text(118, 240, "", levelnameStyle);
 
-        this.refresh();
+        this.add.text(100, 100, "5beam (the custom level server) is currently offline\n for upgrades. Check back soon!", levelnameStyle)
+
+        //this.refresh();
     }
 
     // eslint-disable-next-line class-methods-use-this
     update(): void {
         const epochtime = Date.now();
         // console.log(epochtime);
-        epochtimetext.setText(`${epochtime.toString()} // 5beam-explore`);
+        epochtimetext.setText(`${epochtime.toString()} // custom levels`);
     }
 
     refresh(): void {
-        fetch("https://5beam.zapto.org/api/5bhtml")
+        fetch("https://5beam.zelo.dev/api/5bhtml")
             .then((response) => response.json().then((levels: APIData[]) => {
                 infoText.setText("Fetching data...")
                 levels.forEach((level, i) => {
@@ -157,7 +163,7 @@ class exploreScene extends Phaser.Scene {
                         this, 150, (i * 40), level.name, level.author,
                     );
                     newtile.text.on("pointerdown", () => {
-                        fetch(`https://5beam.zapto.org/get/${level.id}`)
+                        fetch(`https://5beam.zelo.dev/get/${level.id}`)
                             .then((response) => response.text().then((data: string) => {
                                 let escapedData = data
                                 escapedData = escapedData.substring(4, escapedData.length - 4)
@@ -192,7 +198,9 @@ class exploreScene extends Phaser.Scene {
                 }
             }))
             .catch((error) => {
-                infoText.setText("Error refreshing! Maybe 5beam is offline!")
+                infoText
+                    .setText("Error refreshing! Maybe 5beam is offline!")
+                    .setColor("#f00")
                 return console.error(error);
             });
         levelelements.length = 0; // empty array
