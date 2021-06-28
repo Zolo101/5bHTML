b313a499c6b8fa68de995ce6ebcb982969656ead
Zolo101 <umadeguy@gmail.com>
2021-06-29 00:04:07 +0100

    Improve alert design

--- src/game/editor/ui/alert.ts
+++ src/game/editor/ui/alert.ts
@@ -13,21 +13,34 @@ class Alert {
 
     render(scene: Phaser.Scene): void {
         const alert = scene.add.container(scene.scale.width / 2, scene.scale.height / 2);
+        // Background
+        alert.add(scene.add.rectangle(0, 0, 1e4, 1e4, 0x222222, 0.7))
         // Base
-        const base = scene.add.rectangle(0, 0, 400, 300, 0x666666)
-        const top = base.getTopCenter();
+        const base = scene.add.rectangle(0, 0, 400, 300, 0x999999)
+        let top = base.getTopCenter();
         alert.add(base)
         // Title
-        alert.add(scene.add.text(top.x, top.y + 10, this.title, textStyle).setOrigin(0.5, 0))
+        const title = scene.add.text(top.x, top.y + 10, this.title, textStyle)
+            .setFontStyle("bold")
+            .setOrigin(0.5, 0)
         // Text
-        alert.add(scene.add.text(base.getTopLeft().x + 10, top.y + 50, this.text, textStyle).setWordWrapWidth(350))
+        const text = scene.add.text(base.getTopLeft().x + 10, top.y + 50, this.text, textStyle).setWordWrapWidth(350)
+        base.displayHeight = 100 + text.height;
+
+        top = base.getTopCenter();
+        title.setY(top.y + 10);
+        text.setY(top.y + 40);
+
+        alert.add(title)
+        alert.add(text)
         // Close
         if (this.closeable) {
-            alert.add(scene.add.text(base.getBottomCenter().x, base.getBottomCenter().y - 30, "Close", textStyle)
+            alert.add(scene.add.text(base.getBottomCenter().x, base.getBottomCenter().y - 35, "Close", textStyle)
                 .setInteractive()
                 .setOrigin(0.5, 0)
                 .setFontSize(24)
-                .setBackgroundColor("#aaaaaa")
+                .setPadding(5, 0)
+                .setBackgroundColor("#777777")
                 .on("pointerdown", () => alert.removeAll(true))
             )
         }
